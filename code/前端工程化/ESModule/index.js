21ab28860375e9fb77d6002910f3144d859f7e2e
lad <ladqmpw@163.com>
2022-10-15 15:52:23 +0800

    新增ESModule模块化创建和引入

---
+++ code/前端工程化/ESModule/index.js
@@ -0,0 +1,8 @@
+import { b as modelue , getA } from './ziyue.mjs'
+import say from './ziyue.mjs'
+import * as esMoude from './ziyue.mjs'
+
+console.log('b = ', modelue)
+console.log(getA, 'a = ', getA())
+console.log(say, '孔子说：', say('这是esmodule的默认导出，只有一个。'))
+console.log(esMoude.b, esMoude.getA, esMoude.default)
